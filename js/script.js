d0e4282ebdc2c4647390be71e2aa6ade1ec78d78
DrInterstellar <50460660+DrInterstellar@users.noreply.github.com>
2020-06-06 02:34:30 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-// js
+// var indexMargin = document.getElementsByClassName('indexmargin');
+// var height = document.getElementById('indextext').getAttribute('height');
+// function setIndexMarginColor(indexmargin, height) {
+//     indexmargin.setAttribute('style', 'height: ' + height + 'px;');
+// };
+// setIndexMarginColor(indexMargin[0], height);
+// setIndexMarginColor(indexMargin[1], height);
+// console.log(height);
